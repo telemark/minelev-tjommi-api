7d06cc9f716c70b2d1517d2598ee44c8b1b412c9
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 13:02:57 +0200

    Implements classes api

--- api/classes.js
+++ api/classes.js
@@ -1,7 +1,25 @@
+const mongo = require('../lib/mongo')
+
 module.exports = async (request, response) => {
   const url = request.url
   const classId = url.split('/')[2]
   const action = url.split('/')[3]
-  const data = { classId, action }
-  response.json(data)
+  let query = false
+
+  if (classId && action && ['students', 'teachers'].includes(action)) {
+    query = {
+      groupIds: classId,
+      type: action === 'students' ? 'student' : 'teacher'
+    }
+  }
+
+  if (query) {
+    const db = await mongo()
+    const tjommi = db.collection(process.env.MONGODB_COLLECTION)
+    const data = await tjommi.find(query).toArray()
+    response.json(data)
+  } else {
+    response.status(400)
+    response.send('No query created')
+  }
 }
