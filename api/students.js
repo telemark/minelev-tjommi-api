9d41c2c5f04834d57bd14c2fbd494c75be99c31e
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 13:26:58 +0200

    Implements students api

--- api/students.js
+++ api/students.js
@@ -1,8 +1,36 @@
+const mongo = require('../lib/mongo')
+
 module.exports = async (request, response) => {
-  const query = await request.query
+  const { name } = await request.query
   const url = request.url
   const username = url.split('/')[2]
   const action = url.split('/')[3]
-  const data = { ...query, username, action }
-  response.json(data)
+  let query = false
+
+  if (name) {
+    query = {
+      type: 'student',
+      fullName: { $regex: name, $options: 'i' }
+    }
+  } else if (username && !action) {
+    query = {
+      type: 'student',
+      username: username
+    }
+  } else if (username && action && ['contactteachers'].includes(action)) {
+    query = {
+      type: 'student',
+      username: username
+    }
+  }
+
+  if (query) {
+    const db = await mongo()
+    const tjommi = db.collection(process.env.MONGODB_COLLECTION)
+    const data = await tjommi.find(query).toArray()
+    response.json(data)
+  } else {
+    response.status(400)
+    response.send('No query created')
+  }
 }
