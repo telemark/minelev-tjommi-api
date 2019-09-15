4240eb34b5587cc6e116c89c0330e0b241ccf8e2
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 12:54:02 +0200

    Implements schools api

--- api/schools.js
+++ api/schools.js
@@ -1,7 +1,33 @@
+const mongo = require('../lib/mongo')
+
 module.exports = async (request, response) => {
   const url = request.url
   const schoolId = url.split('/')[2]
   const action = url.split('/')[3]
-  const data = { schoolId, action }
-  response.json(data)
+  let query = false
+
+  if (!schoolId && !action) {
+    query = {
+      type: 'skole'
+    }
+  } else if (schoolId && !action) {
+    query = {
+      id: schoolId,
+      type: 'skole'
+    }
+  } else if (schoolId && action && ['students', 'teachers'].includes(action)) {
+    query = {
+      groupIds: schoolId,
+      type: action === 'students' ? 'student' : 'teacher'
+    }
+  }
+  if (query) {
+    const db = await mongo()
+    const tjommi = db.collection(process.env.MONGODB_COLLECTION)
+    const data = await tjommi.find(query).toArray()
+    response.json(data)
+  } else {
+    response.status(400)
+    response.send('No query created')
+  }
 }
