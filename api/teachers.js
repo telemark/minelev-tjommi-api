712957e7dbf8bf404cade9d7409bef1690d808b6
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-16 16:48:14 +0200

    Adds missing functions for the teacher api (patch)

--- api/teachers.js
+++ api/teachers.js
@@ -5,22 +5,35 @@ const handleTeachers = async (request, response) => {
   const url = request.url
   const username = url.split('/')[2]
   const action = url.split('/')[3]
-  let query = false
+  const db = await mongo()
+  const tjommi = db.collection(process.env.MONGODB_COLLECTION)
+
   if (!username && !action) {
-    query = {
+    const query = {
       type: 'teacher'
     }
+    const data = await tjommi.find(query).toArray()
+    response.json(data)
   } else if (username && !action) {
-    query = {
+    const query = {
       username: username,
       type: 'teacher'
     }
-  }
-  if (query) {
-    const db = await mongo()
-    const tjommi = db.collection(process.env.MONGODB_COLLECTION)
     const data = await tjommi.find(query).toArray()
     response.json(data)
+  } else if (username && action && ['contactclasses'].includes(action)) {
+    const teacherQuery = {
+      username: username,
+      type: 'teacher'
+    }
+    const teachers = await tjommi.find(teacherQuery).toArray()
+    const teacher = teachers[0]
+    const groupQuery = {
+      memberIds: teacher.id
+    }
+    // TODO: Implement a way to filter out contactclasses
+    const groups = await tjommi.find(groupQuery).toArray()
+    response.json(groups)
   } else {
     response.status(400)
     response.send('No query created')
--- utils/testcall.js
+++ utils/testcall.js
@@ -5,10 +5,13 @@
   const generateToken = require('./generate-token')
   const token = generateToken(process.env.LOGGED_IN_USER)
   axios.defaults.headers.common.Authorization = token
-  const url = 'http://localhost:3000/schools'
   // const url = 'http://localhost:3000/students?name={name}'
   // const url = 'http://localhost:3000/students/{id}'
   // const url = 'http://localhost:3000/students/{id}/contactteachers'
+  // const url = 'http://localhost:3000/teachers'
+  // const url = 'http://localhost:3000/teachers/{id}'
+  // const url = 'http://localhost:3000/teachers/{id}/contactclasses'
+  const url = 'http://localhost:3000/schools'
 
   try {
     const { data } = await axios.get(url)
