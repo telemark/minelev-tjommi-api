fbe8654bc881af9f4719d9c601ea55015762f737
maccyber <jonas.enge@gmail.com>
2019-10-09 15:22:41 +0200

    Adds error basic handling

--- api/students.js
+++ api/students.js
@@ -28,57 +28,70 @@ const handleStudents = async (request, response) => {
       response.json(data.map(student => repackStudent(student, teacher)))
     } catch (error) {
       logger('error', ['api', 'students', 'search by name', error.message])
+      response.status(500)
       response.json(error)
     }
   } else if (username && !action) {
     logger('info', ['api', 'students', 'search by username', username, 'caller', caller, 'start'])
-    const teachers = await getData({ type: 'teacher', username: caller })
-    const teacher = teachers[0]
-    const teachersGroups = new Set(teacher.groupIds)
-    const query = {
-      type: 'student',
-      username: username
+    try {
+      const teachers = await getData({ type: 'teacher', username: caller })
+      const teacher = teachers[0]
+      const teachersGroups = new Set(teacher.groupIds)
+      const query = {
+        type: 'student',
+        username: username
+      }
+      const data = await getData(query)
+      const isMyStudent = student => {
+        const studentsGroups = new Set(student.groupIds)
+        const intersection = new Set([...teachersGroups].filter(groupId => studentsGroups.has(groupId)))
+        return intersection.size > 0
+      }
+      const myStudents = data.filter(isMyStudent)
+      response.json(myStudents.map(student => repackStudent(student, teacher)))
+    } catch (error) {
+      logger('error', ['api', 'students', 'search by username', error.message])
+      response.status(500)
+      response.json(error)
     }
-    const data = await getData(query)
-    const isMyStudent = student => {
-      const studentsGroups = new Set(student.groupIds)
-      const intersection = new Set([...teachersGroups].filter(groupId => studentsGroups.has(groupId)))
-      return intersection.size > 0
-    }
-    const myStudents = data.filter(isMyStudent)
-    response.json(myStudents.map(student => repackStudent(student, teacher)))
   } else if (username && action && ['contactteachers'].includes(action)) {
-    // TODO: Validate this covers everything to get contactteachers
-    const studentQuery = {
-      type: 'student',
-      username: username
+    try {
+      // TODO: Validate this covers everything to get contactteachers
+      const studentQuery = {
+        type: 'student',
+        username: username
+      }
+      const students = await getData(studentQuery)
+      const { kontaktlarergruppeIds, ordenIds, atferdIds } = students[0]
+      const kontaktIds = [...kontaktlarergruppeIds, ...ordenIds, ...atferdIds]
+      const teacherQuery = groupId => {
+        return {
+          type: 'teacher',
+          groupIds: groupId
+        }
+      }
+      const queries = kontaktIds.map(teacherQuery)
+      const jobs = queries.map(query => getData(query))
+      const results = await Promise.all(jobs)
+      const allTeachers = results.reduce((accumulator, current) => {
+        if (current.length > 0) {
+          accumulator.push(...current)
+        }
+        return accumulator
+      }, [])
+      const teachers = allTeachers.reduce((accumulator, current) => {
+        const ids = accumulator.map(item => item.id)
+        if (!ids.includes(current.id)) {
+          accumulator.push(current)
+        }
+        return accumulator
+      }, [])
+      response.json(teachers)
+    } catch (error) {
+      logger('error', ['api', 'students', 'student contactteachers', error.message])
+      response.status(500)
+      response.json(error)
     }
-    const students = await getData(studentQuery)
-    const { kontaktlarergruppeIds, ordenIds, atferdIds } = students[0]
-    const kontaktIds = [...kontaktlarergruppeIds, ...ordenIds, ...atferdIds]
-    const teacherQuery = groupId => {
-      return {
-        type: 'teacher',
-        groupIds: groupId
-      }
-    }
-    const queries = kontaktIds.map(teacherQuery)
-    const jobs = queries.map(query => getData(query))
-    const results = await Promise.all(jobs)
-    const allTeachers = results.reduce((accumulator, current) => {
-      if (current.length > 0) {
-        accumulator.push(...current)
-      }
-      return accumulator
-    }, [])
-    const teachers = allTeachers.reduce((accumulator, current) => {
-      const ids = accumulator.map(item => item.id)
-      if (!ids.includes(current.id)) {
-        accumulator.push(current)
-      }
-      return accumulator
-    }, [])
-    response.json(teachers)
   } else {
     response.status(400)
     response.send('No query created')
